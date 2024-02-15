9e75e11bd9d8c0a1737800546795d208fb979683
David Tahim <davidtahim@gmail.com>
2024-02-15 20:56:34 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Acadêmico</title>
+</head>
+<body>
+    <img src="imagens/logosenacv2.png" alt="logo senac" width="200px" height="auto">
+    <h1>
+        <b>Controle Acadêmico</b>
+    </h1>
+    <p>Bem vindo ao <b>sistema de controle academico</b>.</p> <p>Aqui você poderá consultar suas notas.</p> <p>E também sua <i>frequencia</i>.</p> 
+</body>
+</html>
