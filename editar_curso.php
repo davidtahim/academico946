15d5196cc897fa63e34154a36ba34fabc615943e
David Tahim <davidtahim@gmail.com>
2024-03-11 19:10:14 -0300

    editar curso

--- editar_curso.php
+++ editar_curso.php
@@ -39,8 +39,23 @@ if (isset($_GET['id'])) {
 <body>
 
 <h2>Editar curso</h2>
-<form action="" method="post">
-    <input type="text" name="id" id="id" value="<?php echo $curso['id']; ?>">
+<form action="atualizar_curso.php" method="post">
+   <p> <input type="hidden" name="id" id="id" value="<?php echo $curso['id']; ?>"></p>
+    <p>
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" value="<?php echo $curso['nome']; ?>">
+    </p>
+    <label for="curso">Curso:</label>
+    <input type="radio" name="curso" id="curso_m" value="m" <?php echo $curso['curso'] == 'm' ? 'checked' : '' ; ?>>
+    <label for="curso_m">Manutenção de computadores</label>
+    <input type="radio" name="curso" id="curso_r" value="r" <?php echo $curso['curso'] == 'r' ? 'checked' : '' ; ?>>
+    <label for="curso_r">Redes de computadores</label>
+    <input type="radio" name="curso" id="curso_p" value="p" <?php echo $curso['curso'] == 'p' ? 'checked' : '' ; ?> >
+    <label for="curso_p">Programação de Computadores</label>
+    <input type="radio" name="curso" id="curso_w" value="w" <?php echo $curso['curso'] == 'w' ? 'checked' : '' ; ?>>
+    <label for="curso_w">Programação Web</label>
+  <p> <input type="submit" value="Atualizar"></p>
+  <p> <a href="cadastrocurso.php">Voltar</a></p>
 </form>
     
 </body>
